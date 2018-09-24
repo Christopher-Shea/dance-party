60b02f46eb8ce324cd0d460e31e4e5659e71542f
Christopher-Shea <ctshea1@gmail.com>
2018-09-24 17:37:30 -0500

    refactor provided class in ES6

--- src/init.js
+++ src/init.js
@@ -22,12 +22,16 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
+    
+    window.dancers.push(dancer);
+
     $('body').append(dancer.$node);
   });
+
 });
 
